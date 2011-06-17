051d201ab81794ddedaef1293dfaf8dfee1723c2
ghy <ghy@you-may.cn>
2011-06-17 16:49:04 +0800

    修改

--- gen/cn/youmay/R.java
+++ gen/cn/youmay/R.java
@@ -11,10 +11,21 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int chart=0x7f020000;
+        public static final int chart1=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int img1=0x7f020003;
+        public static final int img2=0x7f020004;
+        public static final int img3=0x7f020005;
+        public static final int img4=0x7f020006;
+        public static final int img5=0x7f020007;
+    }
+    public static final class id {
+        public static final int imageView1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int network=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
